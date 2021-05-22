0bd2742b2a9d68f07b906d5560f7fcaa9e4b2e3a
Fehmi Çitiloğlu <75936061+mirhurss@users.noreply.github.com>
2021-05-22 08:22:16 +0300

    first commit

---
+++ src/main/java/com/example/amigoscodedemo/AmigosCodeDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.amigoscodedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmigosCodeDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AmigosCodeDemoApplication.class, args);
+    }
+
+}
